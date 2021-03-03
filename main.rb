3e84226c64d536ca40c05fd4e3889311134a88c0
krishpranav <krisna.pranav@gmail.com>
2021-03-03 18:28:19 +0530

    done changes

---
+++ main.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+
+require 'rubygems'
+require 'packetfu'
+require 'thread'
+
+# Local Ruby Files
+curdir = File.dirname(__FILE__)
+require curdir + '/arp.rb'
+require curdir + '/dns.rb'
+require curdir + '/lib/lib_trollop.rb'
+
+#------
+# Trollop Command Line Argument Parsing
+#------
+opts = Trollop::options do
+    version "DNS Spoofer V1.0"
+    banner <<-EOS
+DNS Spoofer in Ruby.
+Usage:
+    ruby main.rb [options]
+    EOS
+    
+    opt :host, "Victim IP", :short => "v", :type => :string, :default => "192.168.0.13"			# String --host <s>, default 127.0.0.1
+#    opt :mac, "Victim MAC", :short => "M", :type => :string, :default => "1c:b0:94:7a:8e:32"	# String --mac <s>
+    opt :mac, "Victim MAC", :short => "m", :type => :string, :default => "78:2b:cb:a3:ef:c9"	# String --mac <s>
+    opt :spoof, "Spoofig IP", :short => "s", :type => :string, :default => "96.55.197.75"		# String --spoof <s>, default 70.70.242.254
+    opt :gate, "Gateway", :short => "g", :type => :string, :default => "192.168.0.100"			# String --gate <s>, default 192.168.0.100
+#    opt :route, "Router MAC", :short => "r", :type => :string, :default => "50:39:55:63:17:b4"	# String --route <s>
+    opt :route, "Router MAC", :short => "R", :type => :string, :default => "00:1a:6d:38:15:ff"	# String --route <s>
+    opt :iface, "Interface", :short => "i", :type => :string, :default => "wlp2s0"				# String --iface <s>, default em1
+
+end # Trollop
+
+#------
+# Preparations
+#------
+
+# Check if user is running as root
+raise "Must run as root or `sudo ruby #{$0}`" unless Process.uid == 0
